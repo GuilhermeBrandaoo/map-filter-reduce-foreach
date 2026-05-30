536f3cdae562632b6bec27cb75ce677f236a2a22
GuilhermeBrandaoo <desenvolvedorguilhermebrandao@gmail.com>
2026-05-29 21:18:20 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const cardapio = document.querySelector(".cardapio")
 const mostrarCardapio = document.querySelector(".mostrar")
-let mostrarProdutos = "";
 const price = document.querySelector(".price")
+const total = document.querySelector(".total")
 
 function mostrar(){
+    let mostrarProdutos = "";
         menuOptions.forEach(item => {
             mostrarProdutos += `
                 <div class="card">
                     <img src="${item.src}" alt="">
                     <h2>${item.name}</h2>
-                    <p>R$${item.price}</p>
+                    <p>R$${item.price.toFixed(2)}</p>
                 </div>`;
                 
         })
 
         cardapio.innerHTML = mostrarProdutos;
-
-        console.log(menuOptions)
-        return menuOptions
 }
 
 function desconto(){
@@ -27,10 +25,46 @@ function desconto(){
                 <div class="card">
                     <img src="${item.src}" alt="">
                     <h2>${item.name}</h2>
-                    <p>R$${produtoComDesconto}</p>
+                    <p>R$${produtoComDesconto.toFixed(2)}</p>
                 </div>`;
     }).join("")
 
         cardapio.innerHTML = mostrarProdutos;
+
+}
+
+function soma() {
+    let totalSomaProdutos = "";
+    const totalProdutos = menuOptions.reduce((acumulador, valorAtual) => {
+        return acumulador + valorAtual.price
+    }, 0)
+
+    totalSomaProdutos += `
+        <p class="descontoProduto">Total: R$${totalProdutos.toFixed(2)}</p>`
+
+        cardapio.innerHTML = totalSomaProdutos;
+}
+
+function filtrar(){
+    const lanchesVeganos = menuOptions.filter(lanche => {
+        if (lanche.vegan == true) {
+            return true
+        } else {
+            return false
+        }
+    })
+
+    let mostrarProdutosVeganos = "";
+        lanchesVeganos.forEach(item => {
+            mostrarProdutosVeganos += `
+                <div class="card">
+                    <img src="${item.src}" alt="">
+                    <h2>${item.name}</h2>
+                    <p>R$${item.price.toFixed(2)}</p>
+                </div>`;
+                
+        })
+
+    cardapio.innerHTML = mostrarProdutosVeganos;
 
 }
